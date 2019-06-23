31cd7be5cf956efe3026f0c1bc5ac14800b2fd22
tonyabraham4 <tonyabraham4@gmail.com>
2019-06-23 15:15:42 +0530

    Using local storage - csv

--- userform_new.R
+++ userform_new.R
@@ -1,3 +1,29 @@
+
+
+outputDir <- "E:/01.r_projects/03.shiny/03.user_input_form/reponses"
+
+saveData <- function(data) {
+  data <- t(data)
+  # Create a unique file name
+  fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data),Sys.getenv("USERNAME"))
+  # Write the file to the local system
+  write.csv(
+    x = data,
+    file = file.path(outputDir, fileName), 
+    row.names = FALSE, quote = TRUE
+  )
+}
+
+loadData <- function() {
+  # Read all the files into a list
+  files <- list.files(outputDir, full.names = TRUE)
+  data <- lapply(files, read.csv, stringsAsFactors = FALSE) 
+  # Concatenate all data together into one data.frame
+  data <- do.call(rbind, data)
+  data
+}
+
+
 library(shiny)
 
 # Define the fields we want to save from the form
@@ -35,4 +61,3 @@ shinyApp(
   }
 )
 
-#test
